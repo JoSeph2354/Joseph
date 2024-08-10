7ed8c36ba9c5065cc2474babd30aa080e2c6163d
joseph <jiiyanam@gmail.com>
2024-08-10 20:40:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,175 @@
+   //document.write("Hello world")
+     //   var personal_info
+       // var age =14; // integer
+  //   var fresh_age ="(Fourteen years)"; // string
+    //    var wish= "Happy Birthday user. You are ";
+      //  var goal= true ;// bolean 
+
+     //   alert(wish+age+fresh_age)
+        //var a = 30;
+        //var b = 20;
+        //alert(a+b)
+
+
+      /*  var wish = "happy birthday.";
+        function birthdayWish()
+        {
+            document.write(wish);
+        }
+        birthdayWish();*/
+
+       /* function addition(number1,number2);
+        {
+            alert(number1+number2);
+        }
+        addition(120,240);*/
+
+      
+     /* var sentence1 = " you will be a "
+      var sentence2 = " in "
+      var sentence3 =" and married to "
+      var sentence4 =" with "
+      var sentence5 =" kids "
+      var x= "doctor"
+      var y= "england"
+      var z= "jane"
+      var n= 4
+        function tell_fortune()
+        {
+        alert(sentence1+x+sentence2+y+sentence3+z+sentence4+n+sentence5);
+        }
+        tell_fortune();*/
+
+        //let numbers = ['one','two','three','four','five']
+     // let no = ['six','seven','eight','nine']
+        //numbers.unshift('zero')
+        //numbers.length()
+        //numbers.reverse()
+        //numbers.sort()
+
+        //alert(numbers.concat(no))
+        
+       
+
+       //for(  i=0;  i<numbers.length;  i++){
+        //console.log(i)
+       //}
+
+
+
+
+/*
+       var numbers = [1, 44, 64, 55, 24, 32, 19, 17]
+
+       max = 0
+       
+       for(i=0; i<numbers.length; i++){
+           if(numbers[i]>max){
+               max =numbers[i]
+           }
+       }
+       
+       console.log(max)
+
+
+
+       var numbers = [1, 44, 64, 55, 24, 32, 19, 17]
+
+       
+       min = 64
+       
+       for(i=0; i<numbers.length; i++){
+           if(numbers[i]<min){
+             min= numbers[i]
+           }
+           
+           
+       }
+       
+       console.log(min)
+
+
+
+ var numbers = [1,2,4,5,11,14,17,20,25,23]
+var prime=[]
+      for(i=0; i<numbers.length; i++){
+        if(numbers[i]%2 !=0 & [i]/[i]==1){
+        prime.push(numbers[i])
+      }
+      }
+      console.log(prime)
+
+
+var h1 =  document.querySelector('#Dom')
+var btn = document.querySelector('.button')
+
+
+function changecolor(){
+h1.innerHTML="Js Class"
+}
+
+function changecolors(){
+  h1.innerHTML="Dom in js"
+}
+*/
+
+var divv = document.querySelector("div")
+var divy = document.querySelector("div")
+
+function changeCol(){
+divv.style.backgroundColor="red"
+}
+
+function change(){
+  divy.style.backgroundColor="white"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
